672468d968a42f7aabcdbf407c999085f7ced442
Lacroix Scott <lacroixscott06@gmail.com>
2023-01-01 22:02:27 -0500

    Add files via upload
    Added Social Media Icons to the bottom of website in App.js, App.css, and SocialFollow.js files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-import logo from './logo.svg';
+import logo from './poap.svg';
 import './App.css';
+
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+
+import {
+  faYoutube,
+  faTwitter,
+  faInstagram
+} from "@fortawesome/free-brands-svg-icons";
 
 function App() {
   return (
@@ -7,17 +17,36 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/channel/UCJJ9qkofalVW5wi83ktE6kg"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          No Movies in Hell
+
         </a>
-      </header>
+   
+      
+      
+    <div class="social-container">
+      <p> Visit our Socials </p>
+      <a href="https://www.youtube.com/channel/UCJJ9qkofalVW5wi83ktE6kg"
+        className="youtube social">
+        <FontAwesomeIcon icon={faYoutube} size="2x" />
+      </a>
+      <a href="https://www.twitter.com/movies_hell" className="twitter social">
+        <FontAwesomeIcon icon={faTwitter} size="2x" />
+      </a>
+      <a href="https://www.instagram.com/nomoviesinhell"
+        className="instagram social">
+        <FontAwesomeIcon icon={faInstagram} size="2x" />
+      </a>
+</div>
+
+  </header>
     </div>
   );
 }
